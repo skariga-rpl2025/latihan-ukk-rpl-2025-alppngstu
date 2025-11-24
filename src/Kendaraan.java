6971405eaac7fa881ad58eb1b8406354f0f8736c
alppngstu <alppngstuu@gmail.com>
2025-11-24 13:50:13 +0700

    Kendaraan

---
+++ src/Kendaraan.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Kendaraan {
+    // Atribut privat
+    private String noPolisi;
+    private String merk;
+    private int tahun;
+
+    // Setter
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+
+    // Getter
+    public String getNoPolisi() {
+        return noPolisi;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+}
+
+
